540405004e08156209d39f001863b7257016102b
Sachin-Tharaka <tharakasachin98@gmail.com>
2021-10-02 03:11:29 +0530

    Send mail v1 final code

---
+++ src/SendMail.java
@@ -0,0 +1,50 @@
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendMail {
+    public static void sendMail(String recepient) throws Exception {
+
+        System.out.println("preparing to send mail");
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        String myAccountEmail = "sachintharakaedu@gmail.com";
+        String password = "eduthara@818";
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccountEmail, password);
+            }
+        });
+
+        Message message = prepareMessage(session, myAccountEmail, recepient);
+
+        Transport.send(message);
+        System.out.println("message sent ");
+
+    }
+
+    private static Message prepareMessage(Session session, String myAccountEmail, String recepient) {
+        Message message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject("My first email from Java");
+            message.setText("Hey there You were Added to the branch");
+            return message;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
+
