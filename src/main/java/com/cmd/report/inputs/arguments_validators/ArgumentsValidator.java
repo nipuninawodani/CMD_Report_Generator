3e4c390ceb5a4121c87c3ab04ff084d6257c9ad0
Isuruni Rathnayaka <rathnaya-se18038@stu.kln.ac.lk>
2021-10-02 11:22:27 -0700

    Delete src/main/java/com/cmd/report/inputs/arguments_validators directory

--- src/main/java/com/cmd/report/inputs/arguments_validators/ArgumentsValidator.java
+++
@@ -1,7 +0,0 @@
-package com.cmd.report.inputs.arguments_validators;
-
-public interface ArgumentsValidator {
-
-    void validateArgument() throws InvalidInputException;
-
-}
--- src/main/java/com/cmd/report/inputs/arguments_validators/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package com.cmd.report.inputs.arguments_validators;
-
-public class InvalidInputException extends Throwable {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
