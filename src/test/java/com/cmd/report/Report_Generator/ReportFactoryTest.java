5bfacfa5d60fd0cf31433c2b2523e7053252d671
Abdullah Ramees <m.abdullah.ramees0916@gmail.com>
2021-10-02 22:09:55 +0530

    report factory

--- src/test/java/com/cmd/report/Report_Generator/ReportFactoryTest.java
+++ src/test/java/com/cmd/report/Report_Generator/ReportFactoryTest.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class reportFactoryTest {
+public class ReportFactoryTest {
     //If  ReportType is user_login should return a object of Userloginreport class.
     @Test
     public void should_return_Userloginreport_object_when_reporttype_is_user_login() {
         ReportFactory reportFactory = new ReportFactory();
-        Report report = reportFactory.reportGeneration("user_login");
+        Report report = reportFactory.reportGeneration("user_login_report");
 
         //verify that the report object is an instance of UserLoginReport class
         assertThat(report, instanceOf(UserLoginReport.class));
@@ -20,7 +20,7 @@ public class reportFactoryTest {
     public void should_return_OrderReport_object_when_ReportType_is_order() {
     //If  ReportType is order should return a object of OrderReport class.
     ReportFactory reportFactory = new ReportFactory();
-    Report report = reportFactory.reportGeneration("user_login");
+    Report report = reportFactory.reportGeneration("order_report");
 
     //verify that the report object is an instance of OrderReport class
     assertThat(report, instanceOf(OrderReport.class));
