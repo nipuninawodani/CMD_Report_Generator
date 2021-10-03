ec76c38b3ae799bd3a8505c9a378f16e614c5cb4
maheladissa <62756370+maheladissa@users.noreply.github.com>
2021-10-03 17:28:16 +0530

    Wrote Unit Tests for SQL Connector

--- src/test/main/java/com/cmd/report/connector/MySqlDBConnectorTest.java
+++ src/test/main/java/com/cmd/report/connector/MySqlDBConnectorTest.java
@@ -1,7 +1,31 @@
 package com.cmd.report.connector;
 
+import org.junit.Test;
+
+import java.sql.Connection;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
 public class MySqlDBConnectorTest {
 
+    //should return connection object when valid inputs are given
+    @Test
+    public void returns_connection_when_arguments_are_correct() throws DBConnectorException {
 
-  
+        DBConnector dbConnector = new MySqlDBConnector();
+        Connection connection = dbConnector.getConnection();
+        assertThat(connection, instanceOf(java.sql.Connection.class));
+    }
+
+    //Should throw an error when invalid argumants are given
+    /*@Test
+    public void throw_an_error_when_invalid_argumants_are_given() {
+
+        DBConnector dbConnector = new MySqlDBConnector();
+        assertThrows(DBControllerException.class, () ->{
+            dbController.getResults(connection,query);
+        });
+
+    }*/
 }
